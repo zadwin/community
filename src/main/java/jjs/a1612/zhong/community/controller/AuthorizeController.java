9d67421d972d99e382d9206f3320637a15d8f2de
zadwin <1324418451@qq.com>
2020-05-10 00:17:32 +0800

    modified AuthorizeController

--- src/main/java/jjs/a1612/zhong/community/controller/AuthorizeController.java
+++ src/main/java/jjs/a1612/zhong/community/controller/AuthorizeController.java
@@ -1,7 +1,9 @@
 package jjs.a1612.zhong.community.controller;
 
+import jjs.a1612.zhong.community.UserMapper.UserMapper;
 import jjs.a1612.zhong.community.dto.AccessTokenDTO;
 import jjs.a1612.zhong.community.dto.GithubUser;
+import jjs.a1612.zhong.community.model.User;
 import jjs.a1612.zhong.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * 为了去获取GitHub的登录授权码
@@ -23,8 +26,11 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 //    它的功能是把写好的Spring 容器中的实例化的实例加载到当前的上下文。
+//    但是这里也涉及到类的生成方式，例如构造函数之类的。
     @Autowired
-    GithubProvider githubProvider;
+    private GithubProvider githubProvider;
+    @Autowired(required=false)
+    private UserMapper userMapper;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state,
@@ -36,12 +42,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user!=null){
-            request.getSession().setAttribute("user",user);
-            return "redirect:/";
-        }else{
-            return "redirect:/";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
         }
+        return "redirect:/";
     }
 }
